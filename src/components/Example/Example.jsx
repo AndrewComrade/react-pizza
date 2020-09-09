a43615ce31a858fee257364b40e991e4100c72d5
AndrewComrade <redcomradefl@gmail.com>
2020-09-09 12:05:46 +0300

    Start

---
+++ src/components/Example/Example.jsx
@@ -0,0 +1,7 @@
+import './Example.scss'
+import React, {Component} from 'react';
+
+
+export class Example extends Component {
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,9 @@ const HTMLPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: path.resolve(__dirname, 'src', 'index.jsx'),
+	entry: path.resolve(__dirname, 'src', 'index.js'),
 	output: {
-		filename: 'bundle[chunkhash].js',
+		filename: 'bundle[hash].js',
 		path: path.resolve(__dirname, 'dist')
 	},
 	devServer: {
@@ -35,6 +35,18 @@ module.exports = {
 					'css-loader',
 					'sass-loader'
 				],
+			},
+			{
+				test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'fonts/'
+						}
+					}
+				]
 			},
 			{
 				test: /\.jsx?$/,
